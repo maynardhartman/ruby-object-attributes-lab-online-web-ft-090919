63093a5ecf434d79e56cdb5cf7f6c77f8281ab3e
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-29 04:51:22 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog 
+  def name=(dog_name)
+    @this_is_the_dogs_name = dog_name
+  end
+  def name 
+    @this_is_the_dogs_name
+  end
+  
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+  def breed
+    @breed
+  end
+  
+end
